1611210f2bfb69e6d80d4e7da0923edd8a320422
Weslen Mendes <weslenmends@gmail.com>
2022-08-17 06:50:56 -0300

    chore: added environment variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 import { getItem } from "../utils/localStorageUtils.js";
 
